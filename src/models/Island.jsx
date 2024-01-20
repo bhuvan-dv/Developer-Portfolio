b9fd13fed18ddcf7f63d6a32c47ae95e38e4b1ba
bhuvan-dv <damavenkateshraj.b@northeastern.edu>
2024-01-20 18:03:03 -0500

    minor islandScene fix

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -14,7 +14,7 @@ import islandScene from "../assets/3d/island.glb"
 
 const Island = (props) => {
     const islandRef = useRef();
-    const { nodes, materials } = useGLTF("/island.glb");
+    const { nodes, materials } = useGLTF(islandScene);
     return (
         <a.group {...props} dispose={null}>
             <mesh
