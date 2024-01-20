60b72bac6343c034600c29df329660b83540ae47
bhuvan-dv <damavenkateshraj.b@northeastern.edu>
2024-01-20 17:28:35 -0500

    Pages and router dom setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+{/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'></div> */}
+const Home = () => {
+  return (
+    <section className='w-full h-screen relative'>
+    </section>
+  )
+}
+
+export default Home
