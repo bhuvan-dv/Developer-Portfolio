75e18b4220dc0bf74c485239727c24026e094ca5
bhuvan-dv <damavenkateshraj.b@northeastern.edu>
2024-01-23 23:43:12 -0500

    feat:company logo

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -4,6 +4,8 @@ import shopify from './shopify.png'
 import starbucks from './starbucks.png'
 import tesla from './tesla.png'
 import logo from './logo.svg'
+import idexcel from "./idexcel.jpeg"
+import testyantra from "./test_yantra_software_solutions_logo.jpeg"
 
 export {
     hero,
@@ -11,5 +13,7 @@ export {
     shopify,
     starbucks,
     tesla,
-    logo
+    logo,
+    idexcel,
+    testyantra
 }
