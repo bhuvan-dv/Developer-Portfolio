4d674bc809c90f6702539007f0cdad4f4eecdb62
bhuvan-dv <damavenkateshraj.b@northeastern.edu>
2024-01-25 16:43:07 -0500

    feat: projects overview

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import { Home, About, Contact, Projects } from "./pages"
 const App = () => {
     return (
         <>
-            <main className='bg-slate-300/20'>
+            <main className='bg-slate-300/20 h-full'>
                 <Router>
                     <Navbar></Navbar>
                     <Routes>
                         <Route path="/" element={<Home />} />
                         <Route path="/about" element={<About />} />
-                        <Route path="/projects" element={<Projects />} />
+                        <Route path="/project" element={<Projects />} />
                         <Route path="/contact" element={<Contact />} />
                     </Routes>
                 </Router>
