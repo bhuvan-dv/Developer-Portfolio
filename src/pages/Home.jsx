dbb0c7a8ab052a64b342c8e645780f2806461492
bhuvan-dv <damavenkateshraj.b@northeastern.edu>
2024-01-25 16:43:24 -0500

    feat: dragon audio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useState } from 'react'
+import React, { Suspense, useState, useEffect, useRef } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 import Island from '../models/Island'
@@ -6,9 +6,17 @@ import Sky from '../models/Sky'
 import Bird from '../models/Bird'
 import Plane from '../models/Plane'
 import HomeInfo from '../components/HomeInfo'
+import sakura from "../assets/sakura.mp3"
+import { soundoff, soundon } from '../assets/icons'
+
 const Home = () => {
+  const audioRef = useRef(new Audio(sakura));
+  audioRef.current.volume = 0.4
+  audioRef.current.loop = true
   const [isRotating, setIsRotating] = useState(false);
   const [currentStage, setCurrentStage] = useState(1);
+  const [isPlayingMusic, setIsPlayingMusic] = useState(false);
+
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
     let screenPosition = [0, -6.5, -43];
@@ -37,6 +45,13 @@ const Home = () => {
 
   const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
   const [planeScale, planePosition] = adjustPlaneForScreenSize();
+
+  useEffect(() => {
+    if (isPlayingMusic) { audioRef.current.play() }
+    return () => {
+      audioRef.current.pause()
+    }
+  }, [isPlayingMusic])
 
   return (
     <section className='w-full h-screen relative'>
@@ -73,6 +88,13 @@ const Home = () => {
             rotation={[0, 20, 0]} />
         </Suspense>
       </Canvas>
+      <div className='absolute bottom-2'>
+        <img
+          src={!isPlayingMusic ? soundoff : soundon}
+          alt="sound"
+          className='w-10 h-10 cursor-pointer object-contain'
+          onClick={() => setIsPlayingMusic(!isPlayingMusic)} />
+      </div>
     </section>
   )
 }
